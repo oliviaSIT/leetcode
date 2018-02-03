00426b7b42f442bd167ed02d5689f270757fb12c
oliviaSIT <jli133@stevens.edu>
2018-02-03 16:19:29 +0000

    solution1 is wrong(15min) new solution is right

---
+++ 344ReverseString.cpp
@@ -0,0 +1,47 @@
+/*https://leetcode.com/problems/reverse-string/description/
+* time:15min
+*/
+
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+/* char -> string?
+class Solution {
+public:
+	string reverseString(string s) {
+        	vector<char> reverse;
+		for(size_t i = s.length(); i >= 0; i--) {
+			reverse.push_back(s[i]);
+		}
+
+		string rs(reverse[0]);
+		for(int i = 1; i < s.length(); i++) {
+			rs = rs + (string)reverse[i];
+		}
+		return rs;
+	}
+};
+*/
+
+class Solution {
+public:
+        string reverseString(string s) {
+		size_t i = 0, j = s.length() - 1;
+		while  (i < j) {
+			swap(s[i], s[j]);
+			i++;
+			j--;
+		}
+		return s;
+	}
+};
+
+
+int main() {
+        Solution sol;
+        cout << sol.reverseString("hello") << endl;
+
+        return 0;
+}
