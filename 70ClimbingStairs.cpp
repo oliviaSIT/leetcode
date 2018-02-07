6e096c69c1f9bb05d6d0cacaaed50d75700738b8
oliviaSIT <jli133@stevens.edu>
2018-02-07 02:25:54 +0000

    2/6/2018 fibbonacci

---
+++ 70ClimbingStairs.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+class Solution {
+public:
+        int climbStairs(int n) {
+                int StepOne = 1;
+        	int StepTwo = 0;
+        	int ret = 0;
+        	for (int i = 0;i < n;i++) {
+                	ret = StepOne + StepTwo;
+            		StepTwo = StepOne;
+            		StepOne = ret;
+        	}
+        
+		return ret;
+        }
+};
