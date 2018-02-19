af13cb1ac3f558245c0ce0b25470a40326e8c676
oliviaSIT <jli133@stevens.edu>
2018-02-19 16:34:38 +0000

    2/19/2018 bit math

---
+++ 29DivideTwoIntegers.cpp
@@ -0,0 +1,46 @@
+/*https://leetcode.com/problems/divide-two-integers/
+ *Medium
+ *time:53min
+ *solution: dividend = 2^k1 * divisor + 2^k2 * divisor......
+ */
+
+#include <iostream>
+#include <algorithm>
+#include <climits>
+
+using namespace std;
+
+class Solution {
+public:
+	int divide(int dividend, int divisor) {
+		if (divisor == 0 || (dividend == INT_MIN && divisor == -1))
+			return INT_MAX;
+
+		int sign = 0;
+		if ((divident < 0) ^ (divisor < 0)) 
+			sign = 1;
+		
+		long long m = labs(dividend);
+		long long n = labs(divisor);
+
+		int result = 0;
+		while (m >= n) {
+			long long temp = n, bit = 1;
+			while (m >= (temp << 1)) {
+				temp <<= 1;
+				bit <<= 1;
+			}
+
+			m -= temp;
+			result += bit;
+		}
+
+		if (sign == 0) {
+			return result;
+		} else {
+			return -result;
+		}
+	}
+};
+
+
