6bfe85df749e2dcd9195326cbb0e508bf8055313
oliviaSIT <jli133@stevens.edu>
2018-02-21 02:03:06 +0000

    2/20/2018 717. 1-bit and 2-bit Characters

---
+++ 717Characters.cpp
@@ -0,0 +1,37 @@
+/*https://leetcode.com/problems/1-bit-and-2-bit-characters/discription
+ *time:
+ *solution:
+ *easy
+ */
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+	bool isOneBitCharacter(vector<int>& b) {
+		bool bit;
+
+		int i = 0;
+		while (i < (int)b.size()) {
+			if (b[i] == 1) {
+				bit = 0;
+				i += 2;
+			} else {
+				bit = 1;
+				i++;
+			}
+		}
+
+		return bit;
+	}
+};
+
+int main() {
+	Solution sol;
+	vector<int> b{1, 1, 1, 0};
+	cout << sol.isOneBitCharacter(b) << endl;
+	return 0;
+}
