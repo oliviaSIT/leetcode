c8fae97b5878134a23468f44f391f61154c85fef
oliviaSIT <jli133@stevens.edu>
2018-03-01 00:29:07 +0000

    2/28/2018

---
+++ 605CanPlaceFlowers.cpp
@@ -0,0 +1,28 @@
+/*https://leetcode.com/problems/can-place-flowers/description/
+ *time:
+ *solution:
+ *easy
+ */
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+	bool canPlaceFlowers(vector<int>& flowerbed, int n) {
+		for (int i = 0; i < (int)flowerbed.size(); i++) {
+			if (flowerbed[i] == 0 && (i == 0 || flowerbed[i - 1] == 0) && (i == flowerbed.size() - 1 || flowerbed[i + 1] == 1)) {
+				flowerbed[i] = 1;
+				n--;
+			}
+
+			if (n <= 0)
+				return true;
+		}
+		if (n <= 0)
+                        return true;
+		else return false;
+	}
+};
