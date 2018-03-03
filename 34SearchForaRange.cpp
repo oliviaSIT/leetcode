856f7729b3ddfbc4aec80929c6cd22ac07149e04
oliviaSIT <jli133@stevens.edu>
2018-03-03 02:11:26 +0000

    3/2/2018 faster

--- 34SearchForaRange.cpp
+++ 34SearchForaRange.cpp
@@ -10,13 +10,58 @@ using namespace std;
 
 class Solution {
 public:
+	vector<int> searchRange(vector<int>& nums, int target) {
+		vector<int> res{-1, -1};
+
+		if (nums.size() == 0 || nums[0] > target || nums[nums.size() - 1] < target)
+			return res;
+
+		int st = 0, ed = nums.size() - 1;
+		int md = findTarget(nums, st, ed, target);
+		res[0] = md;
+		res[1] = md;
+		if (md == -1)
+			return res;
+
+		st = findTarget(nums, 0, md - 1, target);
+		ed = findTarget(nums, md + 1, nums.size() - 1, target);
+		if (st != -1) {
+			res[0] = st;
+			st = findTarget(nums, 0, st - 1, target);
+		}
+
+		if (ed != -1) {
+			res[1] = ed;
+			ed = findTarget(nums, ed + 1, nums.size() - 1, target);
+		}
+
+		return res;
+	}
+
+private:
+	int findTarget(vector<int>& nums, int st, int ed, int target) {
+		if (st > ed)
+			return -1;
+	
+		int md = st + (ed - st) / 2;
+		if (nums[md] == target)
+			return md;
+		else if (nums[md] < target) {
+				return findTarget(nums, md + 1, ed, target);
+			}
+			else return findTarget(nums, st, md - 1, target);
+	}
+};
+/*
+class Solution {
+public:
     vector<int> searchRange(vector<int>& nums, int target) {
         vector<int> res{-1, -1};
         
         if (nums.size() == 0 || nums[0] > target || nums[nums.size() - 1] < target)
             return res;
         
-        int st = 0, ed = 0;
+        int st = 0, ed = nums.size() - 1;
         int md = st + (ed - st) / 2;
         while (st <= ed) {
             if (nums[md] == target) {
@@ -26,8 +71,8 @@ public:
                 while (nums[ed] == target && ed < nums.size())
                     ed++;
                 
-                res[0] = st++;
-                res[1] = ed--;
+                res[0] = ++st;
+                res[1] = --ed;
                 return res;
             } else if (nums[md] < target)
                 st = md;
@@ -37,6 +82,7 @@ public:
         return res;
     }
 };
+*/
 
 int main() {
 	Solution sol;
