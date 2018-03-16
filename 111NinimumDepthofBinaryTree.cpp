a4eaf364b4a19ab290b26ae7ce2ea24e2d3c4597
oliviaSIT <jli133@stevens.edu>
2018-03-16 02:32:36 +0000

    3/15/2018

---
+++ 111NinimumDepthofBinaryTree.cpp
@@ -0,0 +1,14 @@
+/*https://leetcode.com/problems/minimum-depth-of-binary-tree/description
+ *time:17min
+ *solution:
+ *easy
+ */
+class Solution {
+public:
+    int minDepth(TreeNode *root) {
+        if(!root) return 0;
+        if(!root->left) return 1 + minDepth(root->right);
+        if(!root->right) return 1 + minDepth(root->left);
+        return 1+min(minDepth(root->left),minDepth(root->right));
+    }
+};
