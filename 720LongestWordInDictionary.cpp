878a5ab6d4a0cbe468ce817dfb1c21e98a401334
oliviaSIT <jli133@stevens.edu>
2018-03-29 20:04:06 +0000

    3/29/2018

---
+++ 720LongestWordInDictionary.cpp
@@ -0,0 +1,21 @@
+/*https://leetcode.com/problems/longest-word-in-dictionary/description/
+ *time:12min
+ *solution:hash trie
+ *easy
+ */
+
+class Solution {
+public:
+    string longestWord(vector<string>& words) {
+        sort(words.begin(), words.end());
+        unordered_set<string> built;
+        string res;
+        for (string w : words) {
+            if (w.size() == 1 || built.count(w.substr(0, w.size() - 1))) {
+                res = w.size() > res.size() ? w : res;
+                built.insert(w);
+            }
+        }
+        return res;
+    }
+};
