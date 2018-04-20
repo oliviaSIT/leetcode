b12f3ea4fb6dd0cc15a6c1b570fdf6b3478f9fde
oliviaSIT <jli133@stevens.edu>
2018-04-20 02:03:36 +0000

    4/19/2018

---
+++ 173BinarySearchTreeIterator.cpp
@@ -0,0 +1,37 @@
+/*https://leetcode.com/problems/binary-search-tree-iterator/description/
+ *time:31min
+ *solution:
+ *medium
+ */
+
+class BSTIterator {
+public:
+    BSTIterator(TreeNode *root) {
+        while (root) {
+            s.push(root);
+            root = root->left;
+        }
+    }
+
+    /** @return whether we have a next smallest number */
+    bool hasNext() {
+        return !s.empty();
+    }
+
+    /** @return the next smallest number */
+    int next() {
+        TreeNode *n = s.top();
+        s.pop();
+        int res = n->val;
+        if (n->right) {
+            n = n->right;
+            while (n) {
+                s.push(n);
+                n = n->left;
+            }
+        }
+        return res;
+    }
+private:
+    stack<TreeNode*> s;
+};
