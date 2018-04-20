be816614f7646c4e3740617dc5637d55e52bd34e
oliviaSIT <jli133@stevens.edu>
2018-04-20 01:53:02 +0000

    4/19/2018

---
+++ NQueensII.cpp
@@ -0,0 +1,55 @@
+/*https://leetcode.com/problems/n-queens-ii/description/
+ *time:68min
+ *solution:
+ *hard
+ */
+
+#include <iostream>
+#include <vector>
+#include <cstdlib>
+
+using namespace std;
+
+class Solution {
+public:
+	int totalNQueens(int n) {
+		vector<int> col;
+		int totSol = 0;
+		solveNQ(n, 0, col, totSol);
+		return totSol;
+	}
+    
+	void solveNQ(int n, int irow, vector<int>& col, int& totSol) {
+		if (irow == n) {
+			totSol++;
+			return;
+		}
+       
+		for (int icol = 0; icol < n; icol++) {
+			if (validPos(col, irow, icol)) {
+				col.push_back(icol);
+				solveNQ(n, irow + 1, col, totSol);
+				col.pop_back();
+			}
+		}
+    
+	}
+    
+	bool validPos(vector<int>& col, int irow, int icol) {
+		if (irow<col.size()) 
+			return false;
+        
+		for (int i = 0; i < col.size(); i++) {
+			if (icol == col[i] || abs(irow-i) == abs(icol-col[i]))
+				return false;
+		}
+        
+		return true;
+	}    
+};
+
+int main() {
+	Solution sol;
+	cout << sol.totalNQueens(4) << endl;
+	return 0;
+}
